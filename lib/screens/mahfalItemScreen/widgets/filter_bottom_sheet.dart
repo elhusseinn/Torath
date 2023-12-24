a97e2d9685f77f1175ae87a3e7899c8ed5b53cde
elhussein <elhusseinabdalaziz1@gmail.com>
2023-12-24 23:24:31 +0200

    filter bottom sheet items

--- lib/screens/mahfalItemScreen/widgets/filter_bottom_sheet.dart
+++ lib/screens/mahfalItemScreen/widgets/filter_bottom_sheet.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:torath/screens/mahfalItemScreen/widgets/filter_item.dart';
 
 import '../../../core/utils/assets_catalog.dart';
+
 class FilterBottomSheet extends StatefulWidget {
   Set<String> filterPlaces;
   Set<String> filterTimes;
-   FilterBottomSheet({super.key , required this.filterTimes, required this.filterPlaces});
+  FilterBottomSheet(
+      {super.key, required this.filterTimes, required this.filterPlaces});
 
   @override
   State<FilterBottomSheet> createState() => _FilterBottomSheetState();
@@ -33,7 +36,10 @@ class _FilterBottomSheetState extends State<FilterBottomSheet> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(color: Colors.red,width: 54.w,),
+              Container(
+                color: Colors.red,
+                width: 54.w,
+              ),
               Text(
                 'فلتر ',
                 textAlign: TextAlign.center,
@@ -44,14 +50,58 @@ class _FilterBottomSheetState extends State<FilterBottomSheet> {
                   fontWeight: FontWeight.w400,
                 ),
               ),
-              SvgPicture.asset(AssetsCatalog.filterExit),
-
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: SvgPicture.asset(AssetsCatalog.filterExit),
+              ),
             ],
-
+          ),
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height*0.189,
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
+            margin: EdgeInsets.only(top: 21.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'الزمان',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.sp,
+                    fontFamily: 'Dubai',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Expanded(
+                  child: _buildGrid(items: widget.filterTimes),
+                )
+              ],
+            ),
           ),
         ],
       ),
+    );
+  }
 
+  Widget _buildGrid({required Set<String> items}) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 6,
+            crossAxisCount: 3,
+            mainAxisSpacing: 10.h,
+            crossAxisSpacing: 10.h,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: FilterItem(item: items.toList()[index]),
+            );
+          }),
     );
   }
 }
